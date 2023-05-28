894f8a2f7e66f8b877c7313c67947a0fcd0061d7
Dinho Stork <dinhostork@live.com>
2023-05-28 18:23:42 -0300

    test: postfile

---
+++ __tests__/main/models/postfile.spec.ts
@@ -0,0 +1,8 @@
+import PostFile from "../../../src/app/models/PostFile.model";
+
+
+describe("PostFile", () => {
+  it("deve estar definido", () => {
+    expect(PostFile).toBeDefined();
+  });
+});
