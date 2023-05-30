96ce7194529df1b84a459e047adf908ba537be01
Dinho Stork <dinhostork@live.com>
2023-05-30 11:25:57 -0300

    test: #9 deve retornar 201 com os dados da curtida

---
+++ __tests__/http/integration/CurtidasPosts.test.ts
@@ -0,0 +1,59 @@
+import request from "supertest";
+import { Server } from "../../../src/app/server";
+import sequelizeConnection from "../../../src/config/database";
+import User from "../../../src/app/models/User.model";
+import Post from "../../../src/app/models/Post.model";
+import { PRIVACY_PUBLIC } from "../../../src/constants/privacies";
+
+const makePost = async () => {
+  const userdata = {
+    name: "John Doe",
+    email: "email@email.com",
+    password: "12345678",
+  };
+
+  const postdata = {
+    text: "content",
+    privacy_id: PRIVACY_PUBLIC.id,
+  };
+  const user = await User.create(userdata);
+  const post = await Post.create({
+    ...postdata,
+    owner_id: user.id,
+  });
+
+  return post;
+};
+describe("Curtidas em Posts", () => {
+  let server: Server;
+  let app: Express.Application;
+  let token: string;
+  let post: Post;
+
+  beforeAll(async () => {
+    await sequelizeConnection.sync();
+    server = new Server();
+    app = server.start();
+    post = await makePost();
+
+    const response = await request(app).post("/v1/auth").send({
+      email: "email@email.com",
+      password: "12345678",
+    });
+    token = response.body.token;
+  });
+
+  afterEach(async () => {
+    User.destroy({ where: {} });
+    Post.destroy({ where: {} });
+    server.stop();
+  });
+
+  it("deve retornar 201 com os dados da curtida", async () => {
+    const response = await request(app)
+      .post(`/v1/likes/${post.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(201);
+  });
+});
