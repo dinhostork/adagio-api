a71a93eb2823d0459d4c2c07764a33b7290257e9
Dinho Stork <dinhostork@live.com>
2023-06-01 17:11:23 -0300

    feat:  retornar 404 se o post não existir

--- src/app/controllers/PostCommentController.ts
+++ src/app/controllers/PostCommentController.ts
@@ -1,23 +1,36 @@
-import {  Response } from "express";
+import { NextFunction, Response } from "express";
 import { PostCommentService } from "../services/PostComment.service";
 import { ProtectedRequest } from "../interfaces/protectedRequest";
-
+import { PostService } from "../services/Post.service";
+import {  NotFoundError } from "@/utils/errors/httpErrors";
 
 export class PostCommentController {
-    constructor(private readonly postCommentService: PostCommentService) { }
-    async create(req: ProtectedRequest, res: Response) {
-        const { postId } = req.params;
-        const { userId } = req;
-        const { text } = req.body;
+  constructor(
+    private readonly postCommentService: PostCommentService,
+    private readonly postService: PostService
+  ) {}
+  async create(req: ProtectedRequest, res: Response, next: NextFunction) {
+    try {
+      const { postId } = req.params;
+      const { userId } = req;
+      const { text } = req.body;
 
-        const payload = {
-            text,
-            post_id: postId,
-            owner_id: userId!
-        }
+      const post = await this.postService.getPostById(postId);
 
-        const comment = await this.postCommentService.create(payload);
+      if (!post) {
+        throw new NotFoundError("Publicação não encontrada");
+      }
+      const payload = {
+        text,
+        post_id: postId,
+        owner_id: userId!,
+      };
 
-        return res.status(200).json(comment);
+      const comment = await this.postCommentService.create(payload);
+
+      return res.status(200).json(comment);
+    } catch (error) {
+      next(error);
     }
+  }
 }
--- src/routes/comments.router.ts
+++ src/routes/comments.router.ts
@@ -3,6 +3,8 @@ import { PostCommentDao } from "../app/dao/postComment.dao";
 import { autenticated } from "../middlewares/autenticated";
 import { Request, Response, NextFunction, Router } from "express";
 import { PostCommentController } from "../app/controllers/PostCommentController";
+import { PostService } from "../app/services/Post.service";
+import { PostDao } from "../app/dao/post.dao";
 
 const router = Router();
 const slug = "/comments";
@@ -13,8 +15,15 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     const postCommentRepository = new PostCommentDao();
     const postCommentService = new PostCommentService(postCommentRepository);
-    const postCommentController = new PostCommentController(postCommentService);
-    return postCommentController.create(req, res);
+
+    const postRepository = new PostDao();
+    const postService = new PostService(postRepository);
+
+    const postCommentController = new PostCommentController(
+      postCommentService,
+      postService
+    );
+    return postCommentController.create(req, res, next);
   }
 );
 
